54e729545d238cad7ebf053a200a710f49e1178c
EpicSquid <epicsquid315@gmail.com>
2018-11-26 02:35:38 +1100

    Added entity spawning shield effect

---
+++ src/main/java/epicsquid/superiorshields/shield/effect/ShieldEffectSpawn.java
@@ -0,0 +1,37 @@
+package epicsquid.superiorshields.shield.effect;
+
+import java.util.Random;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import epicsquid.superiorshields.capability.IShieldCapability;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.DamageSource;
+import net.minecraft.world.World;
+
+public class ShieldEffectSpawn<E extends Entity> implements IShieldEffect {
+
+  private Random random = new Random();
+  private Class<E> entityClass;
+
+  public ShieldEffectSpawn(@Nonnull Class<E> entityClass) {
+    this.entityClass = entityClass;
+  }
+
+  @Override
+  public void applyEffect(@Nonnull IShieldCapability shield, @Nonnull EntityPlayer player, @Nullable DamageSource source, float damage) {
+    if (!player.world.isRemote && random.nextFloat() < 0.5) {
+      try {
+        Entity entity = entityClass.getConstructor(World.class).newInstance(player.world);
+        entity.setLocationAndAngles(player.posX + random.nextDouble(), player.posY + player.getEyeHeight(), player.posZ + random.nextDouble(), player.rotationYaw,0);
+        player.world.spawnEntity(entity);
+      } catch (Exception e) {
+        // TODO proper exception logging
+        System.out.println("Error: Could not create entity.");
+      }
+
+    }
+  }
+}
