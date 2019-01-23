c47c92308ea99309d02421ca9a0c8be953ccf789
EpicSquid <epicsquid315@gmail.com>
2019-01-23 23:53:17 +1100

    Fixed IF shield not restoring food
    Fixed #6

--- src/main/java/epicsquid/superiorshields/shield/IFShield.java
+++ src/main/java/epicsquid/superiorshields/shield/IFShield.java
@@ -14,7 +14,7 @@ import static epicsquid.superiorshields.config.ConfigManager.industrialForegoing
 public enum IFShield implements IEnergyShield {
 
   PINK_SLIME(industrialForegoing.pinkSlimeMaxHp, industrialForegoing.pinkSlimeRechargeDelay, industrialForegoing.pinkSlimeRechargeRate, industrialForegoing.pinkSlimeEnergy, new ShieldEffectSpawn<>(EntityPinkSlime.class, 0.03f, "shield.effect.spawn.pinkslime")),
-  MEAT(industrialForegoing.meatMaxHp, industrialForegoing.meatRechargeDelay, industrialForegoing.meatRechargeRate, industrialForegoing.meatEnergy, new ShieldEffectFood(0.05f, 1, 1.0f)),
+  MEAT(industrialForegoing.meatMaxHp, industrialForegoing.meatRechargeDelay, industrialForegoing.meatRechargeRate, industrialForegoing.meatEnergy, new ShieldEffectFood(0.3f, 1, 10.0f)),
 
   ;
 
