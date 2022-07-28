b5d9f64c63274be5778e2d075b6dabd8fc42a468
Kat <Kat@sleeping.town>
2022-07-28 14:52:50 -0400

    lol

--- src/main/java/epicsquid/superiorshields/item/SoulStainedSteelShield.java
+++ src/main/java/epicsquid/superiorshields/item/SoulStainedSteelShield.java
@@ -22,8 +22,8 @@ public class SoulStainedSteelShield extends VanillaShieldItem {
 	public Multimap<Attribute, AttributeModifier> getAttributeModifiers(SlotContext slotContext, UUID uuid, ItemStack stack) {
 		var attributes = super.getAttributeModifiers(slotContext, uuid, stack);
 
-		attributes.put(OrtusAttributes.MAGIC_RESISTANCE.get(), new AttributeModifier(UUID.fromString("12b61a11-1633-46db-a22d-45f0b4828cf2"), "Magic Resistance", 1f, AttributeModifier.Operation.ADDITION));
-		attributes.put(AttributeRegistry.SOUL_WARD_CAP.get(), new AttributeModifier(UUID.fromString("a9ac0f44-8e1e-4078-bdea-a11e4c638502"), "Soul Ward Cap", 3f, AttributeModifier.Operation.ADDITION));
+		attributes.put(OrtusAttributes.MAGIC_RESISTANCE.get(), new AttributeModifier(uuid, "Magic Resistance", 1f, AttributeModifier.Operation.ADDITION));
+		attributes.put(AttributeRegistry.SOUL_WARD_CAP.get(), new AttributeModifier(uuid, "Soul Ward Cap", 3f, AttributeModifier.Operation.ADDITION));
 		return attributes;
 	}
 }
