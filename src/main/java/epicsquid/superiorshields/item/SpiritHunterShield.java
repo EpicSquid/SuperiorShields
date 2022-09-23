221a017274ef3cebf93d0698e619d6e22f5151be
EpicSquid <epicsquid315@gmail.com>
2022-09-24 09:27:28 +1000

    Fixed malum 1.4 compatibility

--- src/main/java/epicsquid/superiorshields/item/SpiritHunterShield.java
+++ src/main/java/epicsquid/superiorshields/item/SpiritHunterShield.java
@@ -2,11 +2,11 @@ package epicsquid.superiorshields.item;
 
 import com.google.common.collect.Multimap;
 import com.sammy.malum.core.setup.content.AttributeRegistry;
-import com.sammy.ortus.setup.OrtusAttributes;
 import epicsquid.superiorshields.shield.IShieldType;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.ItemStack;
+import team.lodestar.lodestone.setup.LodestoneAttributeRegistry;
 import top.theillusivec4.curios.api.SlotContext;
 
 import java.util.UUID;
@@ -22,7 +22,7 @@ public class SpiritHunterShield extends VanillaShieldItem {
 	public Multimap<Attribute, AttributeModifier> getAttributeModifiers(SlotContext slotContext, UUID uuid, ItemStack stack) {
 		var attributes = super.getAttributeModifiers(slotContext, uuid, stack);
 
-		attributes.put(OrtusAttributes.MAGIC_PROFICIENCY.get(), new AttributeModifier(uuid, "Magic Proficiency", 1f, AttributeModifier.Operation.ADDITION));
+		attributes.put(LodestoneAttributeRegistry.MAGIC_PROFICIENCY.get(), new AttributeModifier(uuid, "Magic Proficiency", 1f, AttributeModifier.Operation.ADDITION));
 		attributes.put(AttributeRegistry.SCYTHE_PROFICIENCY.get(), new AttributeModifier(uuid, "Scythe Proficiency", 1f, AttributeModifier.Operation.ADDITION));
 		return attributes;
 	}
